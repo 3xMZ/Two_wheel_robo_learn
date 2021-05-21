1149b7dc358c365a7bc77de6735fafb7c0be69e1
Michael <michaelzhang.me@gmail.com>
2021-05-20 21:54:34 -0700

    first commit

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/michael/workspace/catkin_ws/src
+# Build directory: /home/michael/workspace/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("simple_arm")
+subdirs("two_wheel_robo")
---
+++ catkin_ws/build/two_wheel_robo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/michael/workspace/catkin_ws/src/two_wheel_robo
+# Build directory: /home/michael/workspace/catkin_ws/build/two_wheel_robo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
