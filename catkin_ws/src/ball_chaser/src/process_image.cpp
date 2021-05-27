b695a6b0df072340980931c2bd85f891468d9a73
Michael <michaelzhang.me@gmail.com>
2021-05-26 20:12:54 -0700

    Finally got openCV to work.
    Now able to get the RGB values of each pixel.

--- catkin_ws/src/ball_chaser/src/process_image.cpp
+++ catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -30,6 +30,13 @@ void drive_robot(float lin_x, float ang_z)
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
+    cv_bridge::CvImagePtr cv_ptr;
+    cv_ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::BGR8);
+    cv::Mat cv_image = cv_ptr -> image;
+    ROS_INFO_STREAM(std::to_string(cv_image.at<cv::Vec3b>(0,0)[0]));
+    ROS_INFO_STREAM(std::to_string(cv_image.at<cv::Vec3b>(0,0)[1]));
+    ROS_INFO_STREAM(std::to_string(cv_image.at<cv::Vec3b>(0,0)[2]));
+
     int white_pixel = 255;
     int width_mid = img.width / 2;
     int height_mid = img.height / 2;
